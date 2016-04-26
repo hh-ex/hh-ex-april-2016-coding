5323cbf463ef58db85687b8c6f66e54960c35c1d
Glauber Campinho <glauber.campinho@xing.com>
2016-04-26 20:12:38 +0200

    Add `reverse` to `ListOps`

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -3,4 +3,13 @@ defmodule ListOps do
   def count([head | tail]) do
     1 + count(tail)
   end
+
+  def reverse(list) do
+    _reverse(list, [])
+  end
+
+  defp _reverse([], reversed_list), do: reversed_list
+  defp _reverse([head | tail], reversed_list) do
+    _reverse(tail, [head | reversed_list])
+  end
 end
