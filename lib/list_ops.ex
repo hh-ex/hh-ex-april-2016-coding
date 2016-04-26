f76a8b13016492e5448a1e08580f522cd1c79c6d
Bjoern Rochel <bjoern.rochel@xing.com>
2016-04-26 20:00:15 +0200

    Implement map

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -7,6 +7,12 @@ defmodule ListOps do
     reduce(list, [], fn(x, acc) -> [x | acc] end)
   end
 
+  def map(list, mapper) do
+    list
+      |> reduce([], fn(x, acc) -> [mapper.(x) | acc] end)
+      |> reverse
+  end
+
   def reduce(list, acc, step_fun)
   def reduce([h | tail], acc, step_fun) do
     reduce(tail, step_fun.(h,acc), step_fun)
